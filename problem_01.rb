23f2d3fab18f02412f617c072c244bbe76a8d37c
eszter <ekov@protonmail.ch>
2016-12-03 16:57:26 +0100

    Update problem_01.rb

--- problem_01.rb
+++ problem_01.rb
@@ -10,3 +10,24 @@ def calculate_distance_of(path)
   return (distance_in_direction[0] - distance_in_direction[180]).abs +
     (distance_in_direction[90] - distance_in_direction[270]).abs
 end
+
+
+# Part 2
+
+def first_loc_visited_twice_in(path)
+  deg = 0
+  visited_points = []
+  distance_in_direction = { 0 => 0, 90 => 0, 180 => 0, 270 => 0 }
+  path.each do |step|
+    deg = step[0] == 'R' ? ((deg < 270) ? deg + 90 : 0) : ((deg > 0) ? deg - 90 : 270)
+    for block in 1..step[1..-1].to_i
+      distance_in_direction[deg] += 1
+      location = "N#{distance_in_direction[0] - distance_in_direction[180]}, E#{distance_in_direction[90] - distance_in_direction[270]}"
+      visited_points << location
+      if visited_points.count(location) > 1
+        return (distance_in_direction[0] - distance_in_direction[180]).abs +
+          (distance_in_direction[90] - distance_in_direction[270]).abs
+      end
+    end
+  end
+end
