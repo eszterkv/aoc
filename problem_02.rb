2a3bc66c83b061799ee6b7a99217d3a20ade8006
eszter <ekov@protonmail.ch>
2016-12-04 13:19:23 +0100

    Update problem_02.rb

--- problem_02.rb
+++ problem_02.rb
@@ -24,3 +24,27 @@ end
 
 
 # Part 2
+
+def get_code_from(instructions)
+  keypad = [[1],[2,3,4],[5,6,7,8,9],['A','B','C'],['D']]
+  vpos = 2
+  hpos = 0
+  code = ''
+  instructions.each do |step|
+    step.split(//).each do |dir|
+      if dir == 'U' and (vpos > 2 or (hpos > 0 and hpos < keypad[vpos].length - 1))
+        hpos += vpos > 2 ? 1 : -1
+        vpos -= 1
+      elsif dir == 'D' and (vpos < 2 or (hpos > 0 and hpos < keypad[vpos].length - 1))
+        hpos += vpos < 2 ? 1 : -1
+        vpos += 1
+      elsif dir == 'L' and hpos > 0
+        hpos -= 1
+      elsif dir == 'R' and hpos < keypad[vpos].length - 1
+        hpos += 1
+      end
+    end
+    code += keypad[vpos][hpos].to_s
+  end
+  return code
+end
