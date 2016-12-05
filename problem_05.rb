44b4a2b150ce600f87bb0a29b7a151fc88f889c9
eszter <ekov@protonmail.ch>
2016-12-05 12:54:24 +0100

    Update problem_05.rb

--- problem_05.rb
+++ problem_05.rb
@@ -1,6 +1,6 @@
-require 'digest'
-
 input = 'abbhdwsy'
+
+require 'digest'
 
 class Door
   def initialize(door_id)
@@ -11,6 +11,12 @@ class Door
   end
   def password_char
     return @hash[5]
+  end
+  def pw_char_index
+    return '01234567'.index(@hash[5]) != nil ? @hash[5].to_i : false
+  end
+  def pw_char_in_place
+    return @hash[6]
   end
 end
 
@@ -27,4 +33,21 @@ def crack_password(input)
   return pw
 end
 
+def crack_tricky_password(input)
+  pw = '--------'
+  idx = 0
+  while pw.count('-') > 0
+    door = Door.new(input + idx.to_s)
+    if door.has_password_char and door.pw_char_index
+      if pw[door.pw_char_index] == '-'
+        pw[door.pw_char_index] =  door.pw_char_in_place
+        puts pw
+      end
+    end
+    idx += 1
+  end
+  return pw
+end
+
 puts crack_password(input)
+puts crack_tricky_password(input)
