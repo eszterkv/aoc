3a30e2f6d8edff91ec83e5c339e1c9d48d5a33d8
eszter <ekov@protonmail.ch>
2016-12-06 17:24:36 +0100

    Update problem_06.rb

--- problem_06.rb
+++ problem_06.rb
@@ -8,6 +8,8 @@ def decode_message(input)
       letters_count[message[idx]] = letters_count.has_key?(message[idx]) ? letters_count[message[idx]] + 1 : 1
     end
     message[idx] = letters_count.map{|k,v| [k,v]}.select{|x| x[1] == letters_count.map{|k,v| v}.max}.flatten[0]
+    # To find by least common character:
+    # message[idx] = letters_count.map{|k,v| [k,v]}.select{|x| x[1] == letters_count.map{|k,v| v}.min}.flatten[0]
   end
   return message
 end
