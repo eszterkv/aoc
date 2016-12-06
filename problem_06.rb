36ea8e950989f214643cabbc0f70dd9a9f95d830
eszter <ekov@protonmail.ch>
2016-12-06 17:16:30 +0100

    Create problem_06.rb

---
+++ problem_06.rb
@@ -0,0 +1,15 @@
+def decode_message(input)
+  repetitions = input.split("\n")
+  message_length = repetitions[0].length
+  message = '-'*message_length
+  for idx in 0..message_length-1
+    letters_count = Hash.new
+    repetitions.each do |message|
+      letters_count[message[idx]] = letters_count.has_key?(message[idx]) ? letters_count[message[idx]] + 1 : 1
+    end
+    message[idx] = letters_count.map{|k,v| [k,v]}.select{|x| x[1] == letters_count.map{|k,v| v}.max}.flatten[0]
+  end
+  return message
+end
+
+puts decode_message(input)
