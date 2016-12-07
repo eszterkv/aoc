a020684861dc4973f3adb965de6065678f656bf9
eszter <ekov@protonmail.ch>
2016-12-07 17:56:17 +0100

    Update problem_07.rb

--- problem_07.rb
+++ problem_07.rb
@@ -4,9 +4,11 @@ class IPv7
     @strings_outside = address.split(/[\[\]]/).keep_if{|s| address.split(/[\[\]]/).index(s) % 2 == 0}
     @strings_inside = address.split(/[\[\]]/).keep_if{|s| address.split(/[\[\]]/).index(s) % 2 == 1}
   end
+  
   def is_abba(s)
     return (s[0] != s[1] and s[0] == s[3] and s[1] == s[2])
   end
+  
   def supports_tls
     @strings_inside.each do |s|
       for i in 0..s.length-4
@@ -24,6 +26,28 @@ class IPv7
     end
     return false
   end
+  
+  def is_aba(s)
+    return (s[0] != s[1] and s[0] == s[2])
+  end
+  
+  def supports_ssl
+    @strings_outside.each do |s|
+      for i in 0..s.length-3
+        if is_aba(s[i..i+2])
+          @strings_inside.each do |si|
+            for j in 0..si.length-3
+              if si[j..j+2] == s[i+1] + s[i] + s[i+1]
+                return true
+              end
+            end
+          end
+        end
+      end
+    end
+    return false
+  end
+  
 end
 
 def count_tls_ips(input)
@@ -35,4 +59,14 @@ def count_tls_ips(input)
   return count
 end
 
+def count_ssl_ips(input)
+  count = 0
+  input.split("\n").each do |ip|
+    ip = IPv7.new(ip)
+    count += ip.supports_ssl ? 1 : 0
+  end
+  return count
+end
+
 puts count_tls_ips(input)
+puts count_ssl_ips(input)
