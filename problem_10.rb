5fd2078c1fceada238c2b6e8b728cc0a8962906c
eszter <ekov@protonmail.ch>
2016-12-12 09:14:59 +0100

    Update problem_10.rb

--- problem_10.rb
+++ problem_10.rb
@@ -3,20 +3,19 @@ class Bot
     @id = id
     @microchips = []
     @rules = Hash.new
-    @string_rules = ""
   end
 
   def add_chip(value)
     @microchips << value
-    if @microchips.index(17) != nil
-      puts @id, @microchips.to_s
-    end
     if @microchips.count > 1
-      puts @microchips.sort.to_s
-      # if @microchips.sort == $values
-      #   $winner = @id
-      # end
+      self.check_if_wins
       self.dispatch_chips
+    end
+  end
+
+  def check_if_wins
+    if @microchips.sort == $values
+      $winner = @id
     end
   end
 
@@ -37,7 +36,6 @@ class Bot
   attr_reader :id
   attr_reader :microchips
   attr_reader :rules
-  attr_accessor :string_rules
 end
 
 class Output
@@ -47,42 +45,47 @@ class Output
   attr_reader :id
 end
 
+
 def balance_bots(input)
-  bots = Hash.new
+  $bots = Hash.new
   $values = [17, 61]
   $winner = nil
-  instructions = input.split("\n")
-  instructions.each do |i|
-    # puts "\n"
-    # puts i
-    if i.split(" ")[0] == "value"
-      value = i.split(" ")[1].to_i
-      id = i.split(" ")[5]
-      bots[id] = Bot.new(id) unless bots[id]
-      bots[id].add_chip(i.split(" ")[1].to_i)
-      # puts "Bot #{bots[id].id}: #{bots[id].microchips.to_s}, #{bots[id].string_rules}"
+  instructions = input.split("\n").sort_by{|line| line[0]}
+  instructions.each do |instruction|
+    if instruction.split(" ")[0] == "value"
+      add_chip_to_bot_from(instruction)
     else
-      id = i.split(" ")[1]
-      id_low = i.split(" ")[6]
-      id_high = i.split(" ")[11]
-      bots[id] = Bot.new(id) unless bots[id]
-      bots[id].string_rules = i
-      if i.split(" ")[5] == "output"
-        low = Output.new(id_low)
-      else
-        bots[id_low] = Bot.new(id_low) unless bots[id_low]
-        low = bots[id_low]
-      end
-      if i.split(" ")[10] == "output"
-        high = Output.new(id_high)
-      else
-        bots[id_high] = Bot.new(id_high) unless bots[id_high]
-        high = bots[id_high]
-      end
-      bots[id].assign_chips(low, high)
+      assign_low_and_high_from(instruction)
     end
   end
   return $winner
+end
+
+def add_chip_to_bot_from(instruction)
+  value = instruction.split(" ")[1].to_i
+  id = instruction.split(" ")[5]
+  $bots[id] = Bot.new(id) unless $bots[id]
+  $bots[id].add_chip(instruction.split(" ")[1].to_i)
+end
+
+def assign_low_and_high_from(instruction)
+  id = instruction.split(" ")[1]
+  id_low = instruction.split(" ")[6]
+  id_high = instruction.split(" ")[11]
+  $bots[id] = Bot.new(id) unless $bots[id]
+  if instruction.split(" ")[5] == "output"
+    low = Output.new(id_low)
+  else
+    $bots[id_low] = Bot.new(id_low) unless $bots[id_low]
+    low = $bots[id_low]
+  end
+  if instruction.split(" ")[10] == "output"
+    high = Output.new(id_high)
+  else
+    $bots[id_high] = Bot.new(id_high) unless $bots[id_high]
+    high = $bots[id_high]
+  end
+  $bots[id].assign_chips(low, high)
 end
 
 puts balance_bots(input)
