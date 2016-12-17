7e1a2c5cb1e509dd0c55d2ccb5417c3be092542a
eszter <ekov@protonmail.ch>
2016-12-17 16:33:05 +0100

    Update problem_12.rb

--- problem_12.rb
+++ problem_12.rb
@@ -1,27 +1,3 @@
-input = "cpy 1 a
-cpy 1 b
-cpy 26 d
-jnz c 2
-jnz 1 5
-cpy 7 c
-inc d
-dec c
-jnz c -2
-cpy a c
-inc a
-dec b
-jnz b -2
-cpy c b
-dec d
-jnz d -6
-cpy 19 c
-cpy 14 d
-inc a
-dec d
-jnz d -2
-dec c
-jnz c -5"
-
 def decode(input)
   $a = 0
   $b = 0
@@ -77,5 +53,29 @@ def var_from(string)
   end
   return false
 end
+
+input = "cpy 1 a
+cpy 1 b
+cpy 26 d
+jnz c 2
+jnz 1 5
+cpy 7 c
+inc d
+dec c
+jnz c -2
+cpy a c
+inc a
+dec b
+jnz b -2
+cpy c b
+dec d
+jnz d -6
+cpy 19 c
+cpy 14 d
+inc a
+dec d
+jnz d -2
+dec c
+jnz c -5"
 
 puts decode(input)
