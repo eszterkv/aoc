f01d94e82eb750084ad81f01924821293cd3d4ae
eszter <ekov@protonmail.ch>
2016-12-19 17:51:33 +0100

    Update problem_14.rb

--- problem_14.rb
+++ problem_14.rb
@@ -7,11 +7,13 @@ def solve_for(salt)
   keys = []
   while keys.length < 64
     hash = Digest::MD5.new << salt + idx.to_s
+    # hash = hash_2016_times(Digest::MD5.new << salt + idx.to_s, 0)
     if has_3_in_a_row(hash)
       char = has_3_in_a_row(hash).to_s[0]
       next_idx = idx + 1
       while next_idx < idx + 1000
         next_hash = Digest::MD5.new << salt + next_idx.to_s
+        # next_hash = hash_2016_times(Digest::MD5.new << salt + next_idx.to_s, 0)
         if has_5_in_a_row(next_hash, char, next_idx)
           keys << idx
         end
@@ -29,6 +31,15 @@ end
 
 def has_5_in_a_row(hash, char, idx)
   return hash.to_s.index(char * 5) != nil
+end
+
+# very slow, took a few hours to compute
+def hash_2016_times(hash, nth)
+  if nth == 2015
+    return Digest::MD5.new << hash.to_s
+  else
+    return hash_2016_times(Digest::MD5.new << hash.to_s, nth + 1)
+  end
 end
 
 puts solve_for(input)
