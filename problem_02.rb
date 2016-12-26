f5c9aecba6f2ae39a59630ff06f011667dd92a0a
Eszter <ekov@protonmail.ch>
2016-12-26 15:50:47 +0100

    refactor 2

--- problem_02.rb
+++ problem_02.rb
@@ -1,11 +1,13 @@
 # Part 1
 
-def get_code_from(instructions)
-  keypad = [[1,2,3],[4,5,6],[7,8,9]]
+def get_code_from(input)
+  keypad = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
   vpos = 1
   hpos = 1
   code = ''
-  instructions.each do |step|
+  input.split("\n").each do |step|
     step.split(//).each do |ch|
       if ch == 'U' and vpos > 0
         vpos -= 1
@@ -18,19 +20,23 @@ def get_code_from(instructions)
       end
     end
     code += keypad[vpos][hpos].to_s
-  end 
+  end
   return code
 end
 
 
 # Part 2
 
-def get_code_from(instructions)
-  keypad = [[1],[2,3,4],[5,6,7,8,9],['A','B','C'],['D']]
+def get_new_code_from(input)
+  keypad = [[1],
+          [2,3,4],
+        [5,6,7,8,9],
+       ['A','B','C'],
+           ['D']]
   vpos = 2
   hpos = 0
   code = ''
-  instructions.each do |step|
+  input.split("\n").each do |step|
     step.split(//).each do |dir|
       if dir == 'U' and (vpos > 2 or (hpos > 0 and hpos < keypad[vpos].length - 1))
         hpos += vpos > 2 ? 1 : -1
