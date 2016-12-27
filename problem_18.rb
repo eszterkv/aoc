f7c966c27174664e7ba3b80ddd0be7534c9d66e1
Eszter <ekov@protonmail.ch>
2016-12-27 15:57:05 +0100

    add normal speed solution to 18

--- problem_18.rb
+++ problem_18.rb
@@ -1,34 +1,27 @@
 class Tile
-  def initialize(options={})
-    @type = options[:type] || "new"
-    @pos = options[:pos] || "row0"
-    @left = ( (@pos == "row0" or @pos[1] == 0) ? $safe_tile : $tiles[@pos[0]-1][@pos[1]-1] )
-    @center = ( @pos == "row0" ? $safe_tile : $tiles[@pos[0]-1][@pos[1]] )
-    @right = ( (@pos == "row0" or @pos[1] == $line_length-1) ? $safe_tile : $tiles[@pos[0]-1][@pos[1]+1] )
+  def initialize(pos)
+    @left = ( (pos[1] == 0) ? "." : $tiles[pos[0]-1][pos[1]-1] )
+    @center = $tiles[pos[0]-1][pos[1]]
+    @right = ( (pos[1] == $last_idx) ? "." : $tiles[pos[0]-1][pos[1]+1] )
   end
-  def is_trap
-    return @type != "new" ? @type == "^" : ( (@left.is_trap and @center.is_trap and not @right.is_trap) or (@center.is_trap and @right.is_trap and not @left.is_trap) or (@left.is_trap and not (@center.is_trap or @right.is_trap)) or (@right.is_trap and not (@left.is_trap or @center.is_trap)) )
+  def get_str
+    return ( ["^^.", ".^^", "^..", "..^"].include?(@left + @center + @right) ? "^" : "." )
   end
 end
 
 def solve(input, lines)
-  safe_tile_count = input.count(".")
-  $line_length = input.length
-  $safe_tile = Tile.new(type: ".")
-  $tiles = [ input.split(//).map { |char| Tile.new(type: char) } ]
+  $tiles = [input]
+  $last_idx = input.length-1
   while $tiles.length < lines
-    line_of_tiles = []
+    line = ""
     for tile in 0..input.length-1
-      new_tile = Tile.new(pos: [$tiles.length, tile])
-      safe_tile_count += 1 unless new_tile.is_trap
-      line_of_tiles << new_tile
+      line += Tile.new([$tiles.length, tile]).get_str
     end
-    $tiles << line_of_tiles
+    $tiles << line
   end
-  return safe_tile_count
+  return $tiles.join.count(".")
 end
 
 input = ".^.^..^......^^^^^...^^^...^...^....^^.^...^.^^^^....^...^^.^^^...^^^^.^^.^.^^..^.^^^..^^^^^^.^^^..^"
-test = ".^^.^.^^^^"
 
-puts solve(test, 5)
+puts solve(input, 40)
