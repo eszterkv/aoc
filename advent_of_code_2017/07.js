97e40a2b7b2139b7837118f572f79aa85b3ffb78
Eszter <ekov@protonmail.ch>
2017-12-24 12:54:15 +0100

    Super 💩 solution for Pt 2

--- advent_of_code_2017/07.js
+++ advent_of_code_2017/07.js
@@ -13,14 +13,20 @@ class Program {
     return programDef.indexOf('->') > -1
       ? programDef.split('-> ')[1].split(', ') : null;
   }
+  
+  calculateTowerWeight(programs) {
+    this.towerWeight = calculateTowerWeight(programs, this);
+  }
 }
 
 runTest(findRootProgram, tests.suite1);
+runTest(findRootProgramPart2, tests.suite2);
 
 fs.readFile('./inputs/07.input', 'utf8', (err, data) => {
   if (err) throw err;
   const input = data.trim();
   console.log(`Part 1: ${findRootProgram(input)}`);
+  console.log(`Part 2: ${findRootProgramPart2(input)}`);
 });
 
 function findRootProgram(input) {
@@ -33,5 +39,40 @@ function findRootProgram(input) {
 }
 
 function findRootProgramPart2(input) {
-  // part 2 goes here
+  const programs = input.split('\n')
+                        .map((line) => new Program(line));
+  programs.forEach((p) => p.calculateTowerWeight(programs));
+  const root = programs.find((p) => p.name === findRootProgram(input));
+  return(isBalanced(programs, root));
+}
+
+function isBalanced(programs, root) {
+  if (!!root.children) {
+    const children = root.children.map((child) => programs.find((p) => p.name == child));
+    const weights = children.map((c) => c.towerWeight);
+    const balanced = !(children.map((c) => isBalanced(programs, c)).some((p) => p == false))
+      && weights[0] === weights[weights.length - 1];
+    if (!balanced) {
+      const badProgram = children.find((c) => {
+        const badWeight = weights.find((w) => weights.indexOf(w) == weights.lastIndexOf(w));
+        return c.towerWeight === badWeight;
+      });
+      const goodTowerWeight = weights[0] == weights[1] ? weights[0] : weights[weights.length - 1];
+      const diff = goodTowerWeight - badProgram.towerWeight;
+      console.log(badProgram.weight + diff);
+      return badProgram.weight + diff;
+    }
+  }
+}
+
+function calculateTowerWeight(programs, root) {
+  if (!!root.children) {
+    const children = root.children.map((child) => programs.find((p) => p.name == child));
+    const tw = root.weight + children.map((c) => {
+      return calculateTowerWeight(programs, c);
+    }).reduce((prev, curr) => prev + curr);
+    return tw;
+  } else {
+    return root.weight;
+  }
 }
