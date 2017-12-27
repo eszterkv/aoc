62dc47e89eefc7418f2bf4922deb1802f83274a9
Eszter <ekov@protonmail.ch>
2017-12-27 16:55:57 +0100

    Day 8. Uh, a bit late.

---
+++ advent_of_code_2017/08.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const runTest = require('./test');
+const tests = require('./tests/08.test');
+
+class Register {
+  constructor(name) {
+    this.name = name;
+    this.value = 0;
+  }
+  
+  addValue(direction, value) {
+    if (direction === 'inc') {
+      this.value += value;
+    } else this.value -= value;
+  }
+}
+
+runTest(findLargestValue, tests.suite1);
+// runTest(findRootProgramPart2, tests.suite2);
+
+fs.readFile('./inputs/08.input', 'utf8', (err, data) => {
+  if (err) throw err;
+  const input = data.trim();
+  console.log(`Part 1: ${findLargestValue(input)}`);
+  // console.log(`Part 2: ${findLargestValuePt2(input)}`);
+});
+
+function findLargestValue(input) {
+  let registers = [];
+  input.split('\n').map((instruction) => {
+    const name = instruction.split(' ')[0];
+    const register = registers.find((r) => r.name === name) || new Register(name);
+    if (registers.indexOf(register) === -1) registers.push(register);
+
+    const condition = instruction.split('if ')[1];
+    let ok;
+    if (condition) {
+      const reg = registers.find((r) => r.name === condition.split(' ')[0]);
+      const regValue = reg && reg.value || 0;
+      const n = parseInt(condition.split(' ')[2]);
+      switch (condition.split(' ')[1]) {
+        case '==' || '===': ok = regValue == n; break;
+        case '<': ok = regValue < n; break;
+        case '<=': ok = regValue <= n; break;
+        case '>': ok = regValue > n; break;
+        case '>=': ok = regValue >= n; break;
+        case '!=': ok = regValue != n; break;
+      }
+    }
+
+    if (!condition || ok) {
+      const direction = instruction.split(' ')[1];
+      const value = parseInt(instruction.split(' ')[2]);
+      register.addValue(direction, value);
+    }
+  });
+  
+  return registers.find((r) => {
+    return r.value == Math.max.apply(null, registers.map((r) => r.value));
+  }).value;
+}
---
+++ advent_of_code_2017/tests/08.test.js
@@ -0,0 +1,12 @@
+module.exports = {
+  suite1: [
+    {input: `b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10`, expected: 1},
+  ],
+
+  suite2: [
+    {input: ``, expected: 0},
+  ],
+};
