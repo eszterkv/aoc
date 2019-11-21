9f74949979b39e2cec9e01738d1fc52a72053616
Eszter <ekov@protonmail.ch>
2019-11-21 21:45:06 +0000

    06 - build matrix & find valid points

--- 2018/06.js
+++ 2018/06.js
@@ -3,7 +3,7 @@ const runTest = require('./test');
 const tests = require('./tests/06.test');
 
 runTest(solve, tests.suite1);
-runTest(solve2, tests.suite2);
+//runTest(solve2, tests.suite2);
 
 fs.readFile('./inputs/06.input', 'utf8', (err, data) => {
   if (err) throw err;
@@ -11,6 +11,13 @@ fs.readFile('./inputs/06.input', 'utf8', (err, data) => {
 //  console.log(`Part 1: ${solve(input)}`);
 //  console.log(`Part 2: ${solve2(input)}`);
 });
+
+/* get list of coords
+ * find the coord that gives you the largest distance from others?
+ * determine the area around each point
+ *   by counting the locations that are closest to that coord (no ties!)
+ * what is the SIZE of largest area that isn't infinite?
+ */
 
 function solve(input) {
   /*
@@ -21,22 +28,58 @@ function solve(input) {
      5, 5
      8, 9
   */
-  const coords = input.split('\n').map(line => line.split(', ').map(coord => parseInt(coord)));
-  const startX = coords.sort((a, b) => a[0] - b[0])[0][0],
-        startY = coords.sort((a, b) => a[1] - b[1])[0][1],
-        w = coords.sort((a, b) => b[0] - a[0])[0][0] - startX + 1,
-        h = coords.sort((a, b) => b[1] - a[1])[0][1] - startY + 1;
-  console.log(coords, startX, startY, w, h);
+  const coords = parseCoords(input);
+  const startX = coords.reduce((start, curr) => Math.min(start, curr[0]), Infinity),
+        startY = coords.reduce((start, curr) => Math.min(start, curr[1]), Infinity),
+        w = coords.reduce((size, curr) => Math.max(size, curr[0]), -Infinity),
+        h = coords.reduce((size, curr) => Math.max(size, curr[1]), -Infinity);
+
+  console.log(
+    'coords:', coords,
+    '\nstart X and Y:', startX, startY,
+    '\nw, h:', w, h,
+  );
+
+  const matrix = buildMatrix(coords, startX, startY, w, h);
+  printMatrix(matrix);
+
+  const validPoints = pointsWithFiniteArea(coords, matrix, startX, startY, w, h);
+  console.log(validPoints);
+}
+
+function parseCoords(input) {
+  return input.split('\n').map(line => line.split(', ').map(coord => parseInt(coord)));
+}
+
+function buildMatrix(coords, startX, startY, w, h) {
   let row = Array(w).fill('.'),
       matrix = [];
   for (let i = 0; i < h; i++) {
     matrix.push(row);
     row = row.slice(0);
   }
-  coords.forEach((c, i) => {
-    matrix[c[0]][c[1]] = i;
+  coords.forEach((point, idx) => {
+    matrix[point[1] - startX][point[0] - startY] = idx.toString();
   });
-  console.log(matrix);
+
+  return matrix;
+}
+
+function pointsWithFiniteArea(points, matrix, startX, startY, w, h) {
+  return points.filter(point =>
+    point[0] - startX > 0 &&
+    point[0] - startX < w - 1 &&
+    point[1] - startY > 0 &&
+    point[1] - startY < h - 1
+  );
+}
+
+function printMatrix(matrix) {
+  console.log(
+    matrix
+      .map(row => row.join(''))
+      .join('\n')
+  );
 }
 
 function solve2(input) {
