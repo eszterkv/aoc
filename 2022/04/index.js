41561530976a110d3e08f69d02d34d5e86926497
eszter <ekov@protonmail.ch>
2022-12-04 10:49:20 +0100

    🛷 AoC 2022 day 4 refactor

--- 2022/04/index.js
+++ 2022/04/index.js
@@ -1,21 +1,15 @@
 const { parse } = require('../../utils')
 
-const toRanges = pair => pair.split(',').map(range => range.split('-').map(n => parseInt(n)))
-
-const contains = ([min1, max1], [min2, max2]) => min1 <= min2 && max1 >= max2
-
-const overlaps = ([min1, max1], [min2, max2]) => !(min2 > max1 || min1 > max2)
-
 const part1 = input => parse(input).reduce((sum, pair) => {
-  const [range1, range2] = toRanges(pair)
-  if (contains(range1, range2) || contains(range2, range1)) sum++
+  const [min1, max1, min2, max2] = pair.match(/\d/g).map(n => parseInt(n))
+  if (min1 >= min2 && max1 <= max2 || min1 <= min2 && max1 >= max2) sum++
 
   return sum
 }, 0)
 
 const part2 = input => parse(input).reduce((sum, pair) => {
-  const [range1, range2] = toRanges(pair)
-  if (overlaps(range1, range2)) sum++
+  const [min1, max1, min2, max2] = pair.match(/\d/g).map(n => parseInt(n))
+  if (!(min2 > max1 || min1 > max2)) sum++
 
   return sum
 }, 0)
@@ -23,7 +17,4 @@ const part2 = input => parse(input).reduce((sum, pair) => {
 module.exports = {
   part1,
   part2,
-  toRanges,
-  contains,
-  overlaps,
 }
--- 2022/04/index.test.js
+++ 2022/04/index.test.js
@@ -1,4 +1,4 @@
-import { part1, part2, toRanges, contains, overlaps } from '.'
+import { part1, part2 } from '.'
 
 const input = `
 2-4,6-8
@@ -15,18 +15,4 @@ test('part 1', () => {
 
 test('part 2', () => {
   expect(part2(input)).toBe(4)
-})
-
-test('toRanges', () => {
-  expect(toRanges('5-7,7-9')).toEqual([[5, 7], [7, 9]])
-})
-
-test('contains', () => {
-  expect(contains([2, 4], [3, 4])).toBe(true)
-  expect(contains([2, 4], [1, 4])).toBe(false)
-})
-
-test('overlaps', () => {
-  expect(overlaps([2, 4], [3, 9])).toBe(true)
-  expect(overlaps([2, 4], [5, 6])).toBe(false)
 })
