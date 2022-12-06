095d06b69243149c6b13076e0000fea2040ff686
eszter <ekov@protonmail.ch>
2022-12-06 12:56:03 +0100

    🧣 AoC 2022 day 6 - pt2

--- 2022/06/index.js
+++ 2022/06/index.js
@@ -1,15 +1,16 @@
-const part1 = input => {
-  const group = input.substr(0, 4).split('')
+const part1 = (input, markerLength = 4) => {
+  const group = input.substr(0, markerLength).split('')
 
-  for (let i = 4; i < input.length; i++) {
-    if (group.every(ch => group.indexOf(ch) === group.lastIndexOf(ch))) return i
+  for (let i = markerLength; i < input.length; i++) {
+    if (group.every(ch => group.indexOf(ch) === group.lastIndexOf(ch)))
+      return i
+
     group.shift()
     group.push(input[i])
   }
 }
 
-const part2 = input => {
-}
+const part2 = input => part1(input, 14)
 
 module.exports = {
   part1,
--- 2022/06/index.test.js
+++ 2022/06/index.test.js
@@ -14,5 +14,16 @@ test('part 1', () => {
   })
 })
 
-test.skip('part 2', () => {
+const testCases2 = [
+  { input: 'mjqjpqmgbljsphdztnvjfqwrcgsmlb', expected: 19 },
+  { input: 'bvwbjplbgvbhsrlpgdmjqwftvncz', expected: 23 },
+  { input: 'nppdvjthqldpwncqszvftbrmjlhg', expected: 23 },
+  { input: 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', expected: 29 },
+  { input: 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', expected: 26 },
+]
+
+test('part 2', () => {
+  testCases2.forEach(({ input, expected }) => {
+    expect(part2(input)).toBe(expected)
+  })
 })
